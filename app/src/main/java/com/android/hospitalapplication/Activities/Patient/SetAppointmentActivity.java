5ce67376cdcebc16198bf45262ebd2cea3bf291b
Manan <manangupta.cse19@jecrc.ac.in>
2017-12-20 02:49:53 +0530

    set appointment module

--- app/src/main/java/com/android/hospitalapplication/Activities/Patient/SetAppointmentActivity.java
+++ app/src/main/java/com/android/hospitalapplication/Activities/Patient/SetAppointmentActivity.java
@@ -4,14 +4,17 @@ import android.annotation.TargetApi;
 import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.icu.util.Calendar;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.LinearLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -20,6 +23,7 @@ import com.android.hospitalapplication.R;
 
 public class SetAppointmentActivity extends AppCompatActivity {
 Spinner typeofproblem,doctor_list;
+LinearLayout doctordetails;
 Button request_Appointment,calldoctor,preferred_appointment_date;
 TextView doctorcontactnumber,doctoraddress;
     @TargetApi(Build.VERSION_CODES.N)
@@ -29,40 +33,68 @@ TextView doctorcontactnumber,doctoraddress;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_set_appointment);
+           doctordetails=findViewById(R.id.doctordetails);
+        doctor_list = findViewById(R.id.doctor_list);
+        typeofproblem = findViewById(R.id.typeofproblem);
+        request_Appointment = findViewById(R.id.Request_Appointment);
+        preferred_appointment_date = findViewById(R.id.preferred_appointment_date);
+        doctoraddress = findViewById(R.id.doctoraddress);
+        doctorcontactnumber = findViewById(R.id.doctorcontactnumber);
+        calldoctor = findViewById(R.id.calldoctor);
 
-        doctor_list=findViewById(R.id.doctor_list);
-        typeofproblem=findViewById(R.id.doctor_speciality);
-        request_Appointment=findViewById(R.id.Request_Appointment);
-        preferred_appointment_date=findViewById(R.id.preferred_appointment_date);
-        doctoraddress=findViewById(R.id.doctoraddress);
-        doctorcontactnumber=findViewById(R.id.doctorcontactnumber);
-        calldoctor=findViewById(R.id.calldoctor);
+        Calendar cal = Calendar.getInstance();
+        final int year = cal.get(cal.YEAR);
+        final int month = cal.get(cal.MONTH);
+        final int day = cal.get(cal.DAY_OF_MONTH);
 
-            Calendar cal = Calendar.getInstance();
-            final int year = cal.get(cal.YEAR);
-            final int month = cal.get(cal.MONTH);
-            final int day = cal.get(cal.DAY_OF_MONTH);
-
-        initSpinner(typeofproblem, R.array.typeofproblem);
-        initSpinner(doctor_list,R.array.doctorname);
-
+        initSpinner(typeofproblem, R.array.problem);
+        initSpinner(doctor_list, R.array.doctorname);
+      //date picker is set
         preferred_appointment_date.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                DatePickerDialog datepicker=new DatePickerDialog(SetAppointmentActivity.this, new DatePickerDialog.OnDateSetListener() {
+                DatePickerDialog datepicker = new DatePickerDialog(SetAppointmentActivity.this, new DatePickerDialog.OnDateSetListener() {
                     @Override
                     public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
-                        preferred_appointment_date.setText(dayOfMonth+"/"+(month+1)+"/"+year);
+                        preferred_appointment_date.setText(dayOfMonth + "/" + (month + 1) + "/" + year);
                     }
-                },year,month,day);
-                datepicker.show();}
+                }, year, month, day);
+                datepicker.show();
+            }
+        });
+typeofproblem.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+        String problem=typeofproblem.getSelectedItem().toString();
+        if(problem.equals("Cough and Cold")||problem.equals("Fever")||problem.equals("Migrane"))
+        {
+            doctordetails.setVisibility(View.VISIBLE);
+
+        }
+        else if (problem.equals("Ear,Nose and Throat Problem"))
+        {
+
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+});
+
+        //call buuton deatils
+        calldoctor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String doctornumber=doctorcontactnumber.getText().toString();
+                Intent intent=new Intent(Intent.ACTION_DIAL, Uri.fromParts("tel",doctornumber,null));
+                startActivity(intent);
+            }
         });
 
-
-
-
-
-
+          //appointment button is set
         request_Appointment.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
@@ -112,5 +144,6 @@ TextView doctorcontactnumber,doctoraddress;
         adapter.setDropDownViewResource(R.layout.spinner_style);
         s.setAdapter(adapter);
     }
+
 
 }
