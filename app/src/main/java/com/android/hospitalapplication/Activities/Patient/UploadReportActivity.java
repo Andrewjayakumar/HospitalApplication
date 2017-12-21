67848dded00aeb8af52bd45308f98082193055c4
Manan <manangupta.cse19@jecrc.ac.in>
2017-12-22 02:03:04 +0530

    upload report updated

--- app/src/main/java/com/android/hospitalapplication/Activities/Patient/UploadReportActivity.java
+++ app/src/main/java/com/android/hospitalapplication/Activities/Patient/UploadReportActivity.java
@@ -2,24 +2,42 @@ package com.android.hospitalapplication.Activities.Patient;
 
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.android.hospitalapplication.R;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 
 public class UploadReportActivity extends AppCompatActivity {
-    private static final int MY_REQUEST_CODE =1 ;
-    Button capturePhoto,choosePhone;
-    private static final int CAMERA_CAPTURE = 1;
+    Button capturePhoto, choosePhone;
     private Uri fileUri;
     ImageView img;
+    private StorageReference mStorageRef;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,28 +46,35 @@ public class UploadReportActivity extends AppCompatActivity {
         capturePhoto = findViewById(R.id.capturePhoto);
         choosePhone = findViewById(R.id.choosePhone);
         img = findViewById(R.id.img);
+        mStorageRef = FirebaseStorage.getInstance().getReference();
 
         choosePhone.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent galleryIntent = new Intent(Intent.ACTION_PICK,
-                        android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-                // Start the Intent
-                startActivityForResult(galleryIntent, CAMERA_CAPTURE);
+                Intent intent = new   Intent(Intent.ACTION_PICK,android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+
+                startActivityForResult(intent, 2);
+
+                // String selectedImage= fileUri.getPath();
+                //upload(selectedImage);
             }
         });
-        capturePhoto.setOnClickListener(new View.OnClickListener()
-        {
+        capturePhoto.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if( ContextCompat.checkSelfPermission(UploadReportActivity.this, android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                if (ContextCompat.checkSelfPermission(UploadReportActivity.this, android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
                     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
                         requestPermissions(new String[]{android.Manifest.permission.CAMERA},
                                 5);
                     }
                 }
+
                 Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                startActivity(intent);
+                File f = new File(android.os.Environment.getExternalStorageDirectory(), "temp.jpg");
+                String capturedImage = f.getPath().toString();
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(f));
+                startActivityForResult(intent, 1);
+                upload(capturedImage);
             }
 
 
@@ -58,4 +83,138 @@ public class UploadReportActivity extends AppCompatActivity {
 
     }
 
+
+    void upload(String filepath) {
+        Uri file = Uri.fromFile(new File(filepath));
+        String uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        StorageReference report = FirebaseStorage.getInstance().getReference().child("report").child(uid);
+        report.putFile(file)
+                .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        // Get a URL to the uploaded content
+                        String downloadurl = taskSnapshot.getDownloadUrl().toString();
+                        Toast.makeText(UploadReportActivity.this, "Report Uploaded Successfully" + downloadurl, Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception exception) {
+                        // Handle unsuccessful uploads
+                        // ...
+
+                        Toast.makeText(UploadReportActivity.this, "Error Uploading Report", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK) {
+
+            if (requestCode == 1) {
+
+                File f = new File(Environment.getExternalStorageDirectory().toString());
+
+                for (File temp : f.listFiles()) {
+
+                    if (temp.getName().equals("temp.jpg")) {
+
+                        f = temp;
+
+                        break;
+
+                    }
+
+                }
+
+                try {
+
+                    Bitmap bitmap;
+
+                    BitmapFactory.Options bitmapOptions = new BitmapFactory.Options();
+
+
+                    bitmap = BitmapFactory.decodeFile(f.getAbsolutePath(),
+
+                            bitmapOptions);
+
+
+                    img.setImageBitmap(bitmap);
+
+
+                    String path = android.os.Environment
+
+                            .getExternalStorageDirectory()
+
+                            + File.separator
+
+                            + "Phoenix" + File.separator + "default";
+
+                    f.delete();
+
+                    OutputStream outFile = null;
+
+                    File file = new File(path, String.valueOf(System.currentTimeMillis()) + ".jpg");
+
+                    try {
+
+                        outFile = new FileOutputStream(file);
+
+                        bitmap.compress(Bitmap.CompressFormat.JPEG, 85, outFile);
+
+                        outFile.flush();
+
+                        outFile.close();
+
+                    } catch (FileNotFoundException e) {
+
+                        e.printStackTrace();
+
+                    } catch (IOException e) {
+
+                        e.printStackTrace();
+
+                    } catch (Exception e) {
+
+                        e.printStackTrace();
+
+                    }
+
+                } catch (Exception e) {
+
+                    e.printStackTrace();
+
+                }
+
+            } else if (requestCode == 2) {
+
+
+                Uri selectedImage = data.getData();
+                String path=selectedImage.getPath().toString();
+
+                String[] filePath = {MediaStore.Images.Media.DATA};
+
+                Cursor c = getContentResolver().query(selectedImage, filePath, null, null, null);
+
+                c.moveToFirst();
+
+                int columnIndex = c.getColumnIndex(filePath[0]);
+
+                String picturePath = c.getString(columnIndex);
+
+                c.close();
+
+                Bitmap thumbnail = (BitmapFactory.decodeFile(picturePath));
+
+                img.setImageBitmap(thumbnail);
+                upload(path);
+
+            }
+
+        }
+    }
 }
