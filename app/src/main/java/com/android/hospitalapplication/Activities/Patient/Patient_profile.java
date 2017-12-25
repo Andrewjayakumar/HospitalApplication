7c72de2b2e2e86f1a03278149a6c2cdb04020d8a
jon-snow <sarna.kunal07@gmail.com>
2017-12-25 17:13:25 +0530

    changes in view profile

--- app/src/main/java/com/android/hospitalapplication/Activities/Patient/Patient_profile.java
+++ app/src/main/java/com/android/hospitalapplication/Activities/Patient/Patient_profile.java
@@ -2,6 +2,7 @@ package com.android.hospitalapplication.Activities.Patient;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 import com.android.hospitalapplication.R;
 
@@ -14,5 +15,10 @@ public class Patient_profile extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_patient_view_profile);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.profileAppBar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("View profile");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 }
